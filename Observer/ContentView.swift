156b7d22f5592435ce809610202f08818c7de748
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-03-30 15:50:57 -0700

    Add StarCraftKit dependency and tournament loading feature

--- Observer/ContentView.swift
+++ Observer/ContentView.swift
@@ -1,15 +1,43 @@
+import StarCraftKit
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tournaments: [Tournament] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(tournaments.groupedByLeague(), id: \.league.name) { group in
+                    DisclosureGroup {
+                        ForEach(group.tournaments) { tournament in
+                            VStack(alignment: .leading) {
+                                Text(tournament.name)
+                                    .font(.subheadline)
+                                Text(tournament.beginAt.formatted(.relative(presentation: .named)))
+                                    .font(.caption)
+                            }
+                        }
+                    } label: {
+                        Text(group.league.name)
+                            .font(.headline)
+                    }
+                }
+            }
+            .navigationTitle("Past events")
+            .task(load)
         }
-        .padding()
     }
+    
+    @Sendable private func load() async {
+        do {
+            tournaments = try await api.allTournaments().concludedTournaments
+        } catch {
+            print(error)
+        }
+    }
+    
+    private let api = StarCraftAPI(token: ProcessInfo.processInfo.environment["PANDA_TOKEN"]!)
 }
 
 #Preview {
